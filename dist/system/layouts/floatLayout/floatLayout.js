1fb3fa387ddbd076abbffce93731096e80de3640
Azim.Palmer <Azim.Palmer+smartGit@gmail.com>
2015-06-28 15:48:36 +0100

    Fixing and using css :D
    Infrastructure Change: fixing css output

--- dist/system/layouts/floatLayout/floatLayout.js
+++ dist/system/layouts/floatLayout/floatLayout.js
@@ -1,7 +1,7 @@
-System.register(['dash-core', '../../layouts/baseLayout/baseLayout'], function (_export) {
+System.register(['dash-core', '../../layouts/baseLayout/baseLayout', '../../styles/dash-Component.css!css'], function (_export) {
     'use strict';
 
-    var serviceLocator, baseLayout, floatLayout;
+    var serviceLocator, baseLayout, componentcss, floatLayout;
 
     function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
 
@@ -12,6 +12,8 @@ System.register(['dash-core', '../../layouts/baseLayout/baseLayout'], function (
             serviceLocator = _dashCore.serviceLocator;
         }, function (_layoutsBaseLayoutBaseLayout) {
             baseLayout = _layoutsBaseLayoutBaseLayout.baseLayout;
+        }, function (_stylesDashComponentCssCss) {
+            componentcss = _stylesDashComponentCssCss.componentcss;
         }],
         execute: function () {
             floatLayout = (function (_baseLayout) {
--- src/layouts/floatLayout/floatLayout.js
+++ src/layouts/floatLayout/floatLayout.js
@@ -1,5 +1,6 @@
 import {serviceLocator} from 'dash-core'
 import {baseLayout} from '../../layouts/baseLayout/baseLayout'
+import {componentcss} from '../../styles/dash-Component.css!css'
 
 export class floatLayout extends baseLayout
 {
