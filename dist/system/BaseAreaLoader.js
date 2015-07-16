f0cf01ee9c7f8173b9cb815e2bd2a2d1de977991
Azim.Palmer <Azim.Palmer+smartGit@gmail.com>
2015-07-16 04:08:47 +0100

    removed spike scaffolding

--- dist/system/BaseAreaLoader.js
+++ dist/system/BaseAreaLoader.js
@@ -1,47 +1,36 @@
 System.register(['./areas/AreaConfiguration'], function (_export) {
-        'use strict';
+    'use strict';
 
-        var AreaConfiguration, AreaLayout, LayoutConfiguration, BaseAreaLoader;
+    var AreaConfiguration, AreaLayout, LayoutConfiguration, BaseAreaLoader;
 
-        function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+    function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
 
-        return {
-                setters: [function (_areasAreaConfiguration) {
-                        AreaConfiguration = _areasAreaConfiguration.AreaConfiguration;
-                        AreaLayout = _areasAreaConfiguration.AreaLayout;
-                        LayoutConfiguration = _areasAreaConfiguration.LayoutConfiguration;
-                }],
-                execute: function () {
-                        BaseAreaLoader = (function () {
-                                function BaseAreaLoader() {
-                                        _classCallCheck(this, BaseAreaLoader);
-                                }
+    return {
+        setters: [function (_areasAreaConfiguration) {
+            AreaConfiguration = _areasAreaConfiguration.AreaConfiguration;
+            AreaLayout = _areasAreaConfiguration.AreaLayout;
+            LayoutConfiguration = _areasAreaConfiguration.LayoutConfiguration;
+        }],
+        execute: function () {
+            BaseAreaLoader = (function () {
+                function BaseAreaLoader() {
+                    _classCallCheck(this, BaseAreaLoader);
 
-                                BaseAreaLoader.prototype.loadAreaConfiguration = function loadAreaConfiguration(id) {
-                                        var mockAreaResponse = new AreaConfiguration();
-
-                                        mockAreaResponse.areaLayout = new AreaLayout();
-                                        mockAreaResponse.areaLayout.viewModel = 'dash-component/layouts/floatLayout/floatLayout';
-
-                                        var layoutConfig = new LayoutConfiguration();
-                                        layoutConfig.viewModel = 'dash-component/layouts/floatLayout/floatLayout';
-                                        layoutConfig.componentsForCompose = [{ id: 1, viewModel: 'dash-github/components/singleValueComponent/gitSingleValueComponent' }, { id: 1, viewModel: 'dash-github/components/imageGrid/imageGrid' }];
-
-                                        mockAreaResponse.layouts.push(layoutConfig);
-
-                                        layoutConfig = new LayoutConfiguration();
-                                        layoutConfig.viewModel = 'dash-component/layouts/floatLayout/floatLayout';
-                                        layoutConfig.componentsForCompose = [{ id: 1, viewModel: 'components/testComponent/testComponent' }, { id: 1, viewModel: 'components/testComponent/testComponent' }, { id: 1, viewModel: 'components/testComponent/testComponent' }, { id: 1, viewModel: 'components/testComponent/testComponent' }, { id: 1, viewModel: 'components/testComponent/testComponent' }, { id: 1, viewModel: 'components/testComponent/testComponent' }, { id: 1, viewModel: 'components/testComponent/testComponent' }, { id: 1, viewModel: 'components/testComponent/testComponent' }, { id: 1, viewModel: 'components/testComponent/testComponent' }, { id: 1, viewModel: 'components/testComponent/testComponent' }, { id: 1, viewModel: 'components/testComponent/testComponent' }];
-
-                                        mockAreaResponse.layouts.push(layoutConfig);
-
-                                        return Promise.resolve(mockAreaResponse);
-                                };
-
-                                return BaseAreaLoader;
-                        })();
-
-                        _export('BaseAreaLoader', BaseAreaLoader);
+                    this._areaConfigurations = {};
                 }
-        };
+
+                BaseAreaLoader.prototype.addAreaConfiguration = function addAreaConfiguration(id, areaConfig) {
+                    this._areaConfigurations[id] = areaConfig;
+                };
+
+                BaseAreaLoader.prototype.loadAreaConfiguration = function loadAreaConfiguration(id) {
+                    return Promise.resolve(this._areaConfigurations[id]);
+                };
+
+                return BaseAreaLoader;
+            })();
+
+            _export('BaseAreaLoader', BaseAreaLoader);
+        }
+    };
 });
--- src/BaseAreaLoader.js
+++ src/BaseAreaLoader.js
@@ -5,42 +5,16 @@ import {LayoutConfiguration} from './areas/AreaConfiguration'
 
 export class BaseAreaLoader
 {
+
+    _areaConfigurations = {};
+
+    addAreaConfiguration(id,areaConfig)
+    {
+        this._areaConfigurations[id] = areaConfig;
+    }
+
     loadAreaConfiguration(id)
     {
-        var mockAreaResponse = new AreaConfiguration();
-
-        mockAreaResponse.areaLayout = new AreaLayout();
-        mockAreaResponse.areaLayout.viewModel = 'dash-component/layouts/floatLayout/floatLayout';
-
-        var layoutConfig = new LayoutConfiguration();
-        layoutConfig.viewModel = 'dash-component/layouts/floatLayout/floatLayout';
-        layoutConfig.componentsForCompose = [
-            {id: 1, viewModel:'dash-github/components/singleValueComponent/gitSingleValueComponent'},
-            {id: 1, viewModel:'dash-github/components/imageGrid/imageGrid'}
-        ];
-
-        mockAreaResponse.layouts.push(layoutConfig);
-
-
-        layoutConfig = new LayoutConfiguration();
-        layoutConfig.viewModel = 'dash-component/layouts/floatLayout/floatLayout';
-        layoutConfig.componentsForCompose = [
-            {id: 1, viewModel:'components/testComponent/testComponent'},
-            {id: 1, viewModel:'components/testComponent/testComponent'},
-            {id: 1, viewModel:'components/testComponent/testComponent'},
-            {id: 1, viewModel:'components/testComponent/testComponent'},
-            {id: 1, viewModel:'components/testComponent/testComponent'},
-            {id: 1, viewModel:'components/testComponent/testComponent'},
-            {id: 1, viewModel:'components/testComponent/testComponent'},
-            {id: 1, viewModel:'components/testComponent/testComponent'},
-            {id: 1, viewModel:'components/testComponent/testComponent'},
-            {id: 1, viewModel:'components/testComponent/testComponent'},
-            {id: 1, viewModel:'components/testComponent/testComponent'}
-        ];
-
-        mockAreaResponse.layouts.push(layoutConfig);
-
-        return Promise.resolve(mockAreaResponse);
+        return Promise.resolve(this._areaConfigurations[id]);
     }
 }
-
